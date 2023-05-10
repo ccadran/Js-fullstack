c5efee438a5fff30ca4db7afec6f3968f5f4dcc8
ccadran <clariocadran33@gmail.com>
2023-05-10 16:15:59 +0200

    config+ post Schema

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
 const port = 5001;
+
+//Connexion à la base de données
+connectDB();
 
 const app = express();
 
