978e35eb472e29ed87accdb69c4bdb1179287ee4
ccadran <clariocadran33@gmail.com>
2023-05-12 12:02:48 +0200

    start reddux

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -7,7 +7,7 @@ module.exports.getPosts = async (req, res) => {
 
 module.exports.setPosts = async (req, res) => {
   if (!req.body.message) {
-    res.status(400).json({ message: "Merci d'ajouter un message" });
+    return res.status(400).json({ message: "Merci d'ajouter un message" });
   }
 
   const post = await PostModel.create({
