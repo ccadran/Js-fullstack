4c5041336136afcea3dfb0bb8c3efb2cc40cb620
ccadran <clariocadran33@gmail.com>
2023-05-12 12:08:35 +0200

    create user slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import store from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
